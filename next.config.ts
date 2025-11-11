8614aba23add288c75d06d44cf6f7f9c9f96987e
Shane Houghton <shanegh13@gmail.com>
2025-11-10 21:17:13 -0500

    add basepath to next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.PAGES_BASE_PATH,
+  // basePath: process.env.PAGES_BASE_PATH,
+  // Optional: If deploying to a subpath (e.g., your-username.github.io/your-repo-name),
+  // set basePath and assetPrefix. Replace 'your-repo-name' with your repository name.
+  basePath: "/shaneh",
+  assetPrefix: "/shaneh",
 };
 
 export default nextConfig;
