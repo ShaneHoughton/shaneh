e438e026c1653a44ad26b75b90f808302ebef96e
Shane Houghton <shanegh13@gmail.com>
2025-11-11 06:47:00 -0500

    make height full on smaller devices

--- src/app/_components/PageLayout.tsx
+++ src/app/_components/PageLayout.tsx
@@ -12,7 +12,7 @@ interface PageLayoutProps {
 export default function PageLayout(props: PageLayoutProps) {
   const isVisible = useIsVisible();
   return (
-    <div className="font-sans flex flex-col gap-10 items-center sm:items-start m-auto max-h-screen h-[1260px] max-w-[2560px] w-full p-10 sm:p-20">
+    <div className="font-sans flex flex-col gap-10 items-center sm:items-start m-auto max-h-screen h-full sm:h-[1260px] max-w-[2560px] w-full p-10 sm:p-20">
       {props.title && (
         <header className="text-5xl text-center sm:text-left font-mono">
           {props.title}
