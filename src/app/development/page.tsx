2bb6cb162af1911dda749769710b02d989883333
Shane Houghton <shanegh13@gmail.com>
2025-11-14 10:44:21 -0500

    new section on development page

--- src/app/development/page.tsx
+++ src/app/development/page.tsx
@@ -5,6 +5,56 @@ export default function Developement() {
   const title = "development";
   return (
     <PageLayout title={title}>
+      <h2 className="text-3xl font-mono font-medium">
+        AI Saftey and Awareness
+      </h2>
+      <ul className="w-full flex flex-col gap-4 list-disc ml-4 px-2">
+        <li className="text-xl">
+          <a
+            className="hover:underline hover:underline-offset-4 cursor-pointer"
+            href="https://www.youtube.com/watch?v=O7BI4jfEFwA&t=0s"
+            target="_blank"
+          >
+            <span className="font-semibold">DEF CON 33 (youtube)</span> -
+            private data in LLMs
+          </a>
+        </li>
+        <li className="text-xl">
+          <a
+            className="hover:underline hover:underline-offset-4 cursor-pointer"
+            href="https://www.youtube.com/watch?v=5CKuiuc5cJM&t=0s"
+            target="_blank"
+          >
+            <span className="font-semibold">
+              Hank Green interviews Nate Soares (youtube)
+            </span>{" "}
+            - If Anyone Builds It, Everyone Dies
+          </a>
+        </li>
+        <li className="text-xl">
+          <a
+            className="hover:underline hover:underline-offset-4 cursor-pointer"
+            href="https://www.facebook.com/privacy/policy?subpage=1.subpage.1-YourActivityAndInformation"
+            target="_blank"
+          >
+            <span className="font-semibold">META policies</span> - FYI YOUR
+            public info, posts, art, etc, train AI models.
+          </a>
+        </li>
+        <li className="text-xl">
+          <a
+            className="hover:underline hover:underline-offset-4 cursor-pointer"
+            href="https://www.anthropic.com/research/agentic-misalignment"
+            target="_blank"
+          >
+            <span className="font-semibold">
+              Anthropic Agentic Misalignment
+            </span>
+            - Malicious behaviors of LLMs
+          </a>
+        </li>
+      </ul>
+      <h2 className="text-3xl font-mono font-medium">Personal work</h2>
       <ul className="w-full flex flex-col gap-4 list-disc ml-4 px-2">
         <li className="text-xl">
           <a
